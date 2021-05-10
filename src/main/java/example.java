f8147e92257792e7fecb751b6bff1b010d0d81c9
maxim-buryak <buryakwork@gmail.com>
2021-05-10 23:48:03 +0300

    Create work

--- src/main/java/example.java
+++ src/main/java/example.java
@@ -1,6 +1,7 @@
 public class example {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Work");
         System.out.println("By");
     }
 }
